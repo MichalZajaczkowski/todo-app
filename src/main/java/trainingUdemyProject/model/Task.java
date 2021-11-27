e1a9fab31342cec712f1cf898807b1f08e8b0041
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-11-27 16:00:23 +0100

    add dependency hibernate, add 3 filds in Task Class

--- src/main/java/trainingUdemyProject/model/Task.java
+++ src/main/java/trainingUdemyProject/model/Task.java
@@ -1,4 +1,12 @@
 package trainingUdemyProject.model;
 
+import org.hibernate.annotations.Entity;
+import org.hibernate.annotations.Tables;
+
+@Entity
+@Tables(name = "tasks")
 public class Task {
+    private int id;
+    private String description;
+    private boolean done;
 }
