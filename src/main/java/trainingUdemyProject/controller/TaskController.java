7aea09201c009363d69fa988168f1c94299ce3da
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-11-30 20:26:31 +0100

    add logger in TaskController and

--- src/main/java/trainingUdemyProject/controller/TaskController.java
+++ src/main/java/trainingUdemyProject/controller/TaskController.java
@@ -1,7 +1,22 @@
 package trainingUdemyProject.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import trainingUdemyProject.model.TaskRepository;
 
 @RepositoryRestController
 public class TaskController {
+    private static final Log LOG = LogFactory.getLog(TaskController.class);
+    private final TaskRepository taskRepository;
+
+    TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    ResponseEntity<?> readAllTask() {
+        LOG.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(taskRepository.findAll());
+    }
 }
