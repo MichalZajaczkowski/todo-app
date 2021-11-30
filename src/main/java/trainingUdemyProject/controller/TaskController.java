2f1f82a8eec30affd001ddc1bcb410f70ca80a01
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-11-30 21:01:50 +0100

    add GetMapping to ResponseEntity logger

--- src/main/java/trainingUdemyProject/controller/TaskController.java
+++ src/main/java/trainingUdemyProject/controller/TaskController.java
@@ -4,6 +4,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import trainingUdemyProject.model.TaskRepository;
 
 @RepositoryRestController
@@ -14,7 +17,8 @@ public class TaskController {
     TaskController(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
-
+//    @RequestMapping(method = RequestMethod.GET, path = "/tasks")
+    @GetMapping("/tasks")
     ResponseEntity<?> readAllTask() {
         LOG.warn("Exposing all the tasks!");
         return ResponseEntity.ok(taskRepository.findAll());
