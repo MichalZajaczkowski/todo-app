7e0f536ca7f020b8d9ee8467ac357a6e5e7925cb
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-11-30 20:03:18 +0100

    blocking the removal of tasks and filtering them by status

--- src/main/java/trainingUdemyProject/model/TaskRepository.java
+++ src/main/java/trainingUdemyProject/model/TaskRepository.java
@@ -1,8 +1,25 @@
 package trainingUdemyProject.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
+//@RepositoryRestResource(path = "todos", collectionResourceRel = "todos")
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    @Override
+    @RestResource(exported = false)
+    default void deleteById(Integer integer) {
+
+    }
+    @Override
+    @RestResource(exported = false)
+    default void delete(Task entity) {
+    }
+    @RestResource(path = "done", rel = "done")
+    List<Task> findByDone(@Param("state") boolean done);
 }
