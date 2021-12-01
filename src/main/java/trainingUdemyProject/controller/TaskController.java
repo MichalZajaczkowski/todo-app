609f26fe74bb1f9c4ed641b2fa649f05a4ee849b
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-12-01 20:00:33 +0100

    PutMapping in TaskController, updateTask

--- src/main/java/trainingUdemyProject/controller/TaskController.java
+++ src/main/java/trainingUdemyProject/controller/TaskController.java
@@ -1,34 +1,39 @@
 package trainingUdemyProject.controller;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import trainingUdemyProject.model.Task;
-import trainingUdemyProject.model.SqlTaskRepository;
+import trainingUdemyProject.model.TaskRepository;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 public class TaskController {
-    private static final Log LOG = LogFactory.getLog(TaskController.class);
-    private final SqlTaskRepository taskRepository;
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskRepository taskRepository;
 
-    TaskController(SqlTaskRepository taskRepository) {
+    TaskController(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 //    @RequestMapping(method = RequestMethod.GET, path = "/tasks")
     @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
     ResponseEntity<List<Task>> readAllTask() {
-        LOG.warn("Exposing all the tasks!");
+        logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(taskRepository.findAll());
     }
     @GetMapping(value = "/tasks")
-    ResponseEntity<List<Task>> readAllTask(Pageable pageable) {
-        LOG.warn("Custom pageable");
-        return ResponseEntity.ok(taskRepository.findAll(pageable).getContent());
+    ResponseEntity<List<Task>> readAllTask(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(taskRepository.findAll(page).getContent());
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@RequestBody @Valid Task toUpdate){
+        taskRepository.save(toUpdate);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/trainingUdemyProject/model/SqlTaskRepository.java
+++ src/main/java/trainingUdemyProject/model/SqlTaskRepository.java
@@ -1,12 +1,12 @@
 package trainingUdemyProject.model;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
+
+
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
-public interface SqlTaskRepository extends JpaRepository<Task, Integer> {
-    List<Task> findByDone(@Param("state") boolean done);
+public interface SqlTaskRepository extends TaskRepository,  JpaRepository<Task, Integer> {
 }
