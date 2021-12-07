96c5598357df5e9454d76534ef22b8d4c0ac0a51
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-12-07 20:31:52 +0100

    in pom add ...configuration-processor, create TaskConfigurationProperties, add getter add setter

--- src/main/java/trainingUdemyProject/TaskConfigurationProperties.java
+++ src/main/java/trainingUdemyProject/TaskConfigurationProperties.java
@@ -1,4 +1,17 @@
 package trainingUdemyProject;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("task")
 public class TaskConfigurationProperties {
+
+    private boolean allowMultipleTasksFromTemplates;
+
+    boolean isAllowMultipleTasksFromTemplates() {
+        return allowMultipleTasksFromTemplates;
+    }
+
+    void setAllowMultipleTasksFromTemplates(boolean allowMultipleTasksFromTemplates) {
+        this.allowMultipleTasksFromTemplates = allowMultipleTasksFromTemplates;
+    }
 }
