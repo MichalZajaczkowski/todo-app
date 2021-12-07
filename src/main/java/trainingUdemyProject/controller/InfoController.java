03be26be8db664c80954e9fea2175b2511a5e00b
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-12-07 21:51:13 +0100

    delete @Autowired and  @Value("${task.allowMultipleTasksFromTemplates}") from InfoController, changes in InfoController, add @Configuration in TaskConfigurationProperties

--- src/main/java/trainingUdemyProject/controller/InfoController.java
+++ src/main/java/trainingUdemyProject/controller/InfoController.java
@@ -1,7 +1,5 @@
 package trainingUdemyProject.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +7,10 @@ import trainingUdemyProject.TaskConfigurationProperties;
 
 @RestController
 public class InfoController {
-    private DataSourceProperties dataSource;
-    private TaskConfigurationProperties myProp;
+    private final DataSourceProperties dataSource;
+    private final TaskConfigurationProperties myProp;
 
-    InfoController(DataSourceProperties dataSource, TaskConfigurationProperties myProp) {
+    InfoController(final DataSourceProperties dataSource, final TaskConfigurationProperties myProp) {
         this.dataSource = dataSource;
         this.myProp = myProp;
     }
