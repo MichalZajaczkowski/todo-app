5ed8f241ac49fbca4fc03b98477a380247509812
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-12-07 19:45:24 +0100

    add my.prop in application.yml and add new Controller

---
+++ src/main/java/trainingUdemyProject/controller/InfoController.java
@@ -0,0 +1,14 @@
+package trainingUdemyProject.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+    String url() {
+        //
+    }
+
+    String myProp() {
+
+    }
+}
