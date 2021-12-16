3b31a015923ac33dc1c5a001d6773ed93d6f070a
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-12-16 19:18:04 +0100

    delete abstract from Audit

--- src/main/java/trainingUdemyProject/model/Audit.java
+++ src/main/java/trainingUdemyProject/model/Audit.java
@@ -6,7 +6,7 @@ import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
 @Embeddable
-abstract class Audit {
+ class Audit {
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
 
