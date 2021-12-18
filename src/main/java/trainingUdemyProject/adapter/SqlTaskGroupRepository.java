c88d523562e0ecf2649c239c02c65a8974377ade
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-12-18 14:38:42 +0100

    add  @Override TaskGroup in SqlTaskGroupRepository, creat V6__init _project_tables

--- src/main/java/trainingUdemyProject/adapter/SqlTaskGroupRepository.java
+++ src/main/java/trainingUdemyProject/adapter/SqlTaskGroupRepository.java
@@ -1,8 +1,17 @@
 package trainingUdemyProject.adapter;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import trainingUdemyProject.model.TaskGroup;
 import trainingUdemyProject.model.TaskGroupRepository;
 
+import java.util.List;
+
+@Repository
 public interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+
+    @Override
+    @Query("from TaskGroup g join fetch g.tasks")
+    List<TaskGroup> findAll();
 }
