2fd29537a6861d690613923954f82f02aef8de01
Michał Zajączkowski <79537829+MichalZajaczkowski@users.noreply.github.com>
2021-12-18 15:16:14 +0100

    creat ProjectRepository and SqlProjectRepository

--- src/main/java/trainingUdemyProject/adapter/SqlProjectRepository.java
+++ src/main/java/trainingUdemyProject/adapter/SqlProjectRepository.java
@@ -3,18 +3,18 @@ package trainingUdemyProject.adapter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import trainingUdemyProject.model.Project;
+import trainingUdemyProject.model.ProjectRepository;
 import trainingUdemyProject.model.TaskGroup;
 import trainingUdemyProject.model.TaskGroupRepository;
 
 import java.util.List;
 
 @Repository
-public interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+public interface SqlProjectRepository extends ProjectRepository, JpaRepository<Project, Integer> {
 
     @Override
-    @Query("select distinct g from TaskGroup g join fetch g.tasks")
-    List<TaskGroup> findAll();
+    @Query("select distinct p from Project p join fetch p.steps")
+    List<Project> findAll();
 
-    @Override
-    boolean existsByDoneIsFalseAndProject_Id(Integer projectId);
 }
